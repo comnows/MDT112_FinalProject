5c0a3feb0e0f23a71ecac07bbf44df5e69b16eb3
Now Luaeng <watthanandh.l@mail.kmutt.ac.th>
2020-04-18 23:52:29 +0700

    Initialized git repository.

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 Offset;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + Offset;
+
+        transform.Rotate(0,speed * Time.deltaTime,0);
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] KeyCode MoveForward;
+    [SerializeField] KeyCode MoveBackward;
+    [SerializeField] KeyCode MoveLeft;
+    [SerializeField] KeyCode MoveRight;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(MoveForward))
+        {
+            transform.position += new Vector3(0f,0f,0f) * Time.deltaTime;
+        }
+        if(Input.GetKey(MoveBackward))
+        {
+            transform.position += new Vector3(0f,0f,0f) * Time.deltaTime;
+        }
+        if(Input.GetKey(MoveLeft))
+        {
+            transform.position += new Vector3(0f,0f,0f) * Time.deltaTime;
+        }
+        if(Input.GetKey(MoveRight))
+        {
+            transform.position += new Vector3(0f,0f,0f) * Time.deltaTime;
+        }
+    }
+}
