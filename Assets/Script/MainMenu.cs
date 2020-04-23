0ca06f651a055ac1428d5bd2b65c8ace8d6bf8fb
comnows <watthanandh.l@mail.kmutt.ac.th>
2020-04-23 17:23:51 +0700

    Code Update2
    -MainMenu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
