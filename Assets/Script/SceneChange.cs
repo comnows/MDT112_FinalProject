71363c61bced24da62de60971d990530cb683e84
comnows <watthanandh.l@mail.kmutt.ac.th>
2020-05-06 18:54:10 +0700

    BackUp
    -MainMenu
    -code scenechange,fade
    -add some rigidbody and collider

---
+++ Assets/Script/SceneChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+        {
+            if(other.gameObject.name == "StoreDoor") //Change to name of gameObject
+            {
+                SceneManager.LoadScene("MainMenu"); //Change to scene want to load
+            }
+            else if(other.gameObject.name == "BedroomDoor") //Change to name of gameObject
+            {
+                SceneManager.LoadScene("Bedroom"); //Change to scene want to load
+            }
+            else if(other.gameObject.name == "ExitStore") //Change to name of gameObject
+            {
+                SceneManager.LoadScene("LevelSelect"); //Change to scene want to load
+            }
+        }
+}
